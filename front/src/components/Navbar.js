527454fb49e6d5933bb6541aa837915f6104afec
TyFhoon2X3 <6710210473@psu.ac.th>
2025-12-17 00:03:39 +0700

    feat: add Navbar component

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -60,7 +60,7 @@ export default function Navbar() {
     if (role === "r1") {
       return (
         <>
-          <NavLink to="/admin-dashboard">แดชบอร์ดผู้ดูแลระบบ</NavLink>
+          <NavLink to="/admin-dashboard">แดชบอร์ด</NavLink>
           <NavLink to="/admin/customers">ลูกค้า</NavLink>
           <NavLink to="/admin/vehicles">รถของลูกค้า</NavLink>
           <NavLink to="/admin/bookings">บริการ</NavLink>
@@ -72,7 +72,7 @@ export default function Navbar() {
     if (role === "r2") {
       return (
         <>
-          <NavLink to="/user-dashboard">แดชบอร์ดลูกค้า</NavLink>
+          <NavLink to="/user-dashboard">แดชบอร์ด</NavLink>
           <NavLink to="/my-vehicles">รถของฉัน</NavLink>
           <NavLink to="/book-service">จองบริการ</NavLink>
           <NavLink to="/bookings">ประวัติการจอง</NavLink>
